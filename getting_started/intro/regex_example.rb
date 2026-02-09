0ecd5f1e3a125479a868c01e1bc9f06f26fe3bd4
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-09 16:39:17 -0300

    adiciona exemplos iniciais com regex

---
+++ getting_started/intro/regex_example.rb
@@ -0,0 +1,2 @@
+line = gets
+puts "programming language mentioned #{line}" if line.match?(/Ruby|Rust/)
---
+++ getting_started/intro/regex_example2.rb
@@ -0,0 +1,3 @@
+line = gets
+
+puts "maior clube do Mundo é ele #{line}" if line.match?(/SPFC|Barcelona/)
