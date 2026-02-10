b1801b2e4e2a3d1dc12c0fd838400358b8701e7c
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-10 10:49:03 -0300

    adiciona refatoracao adicionando attr_acessor e attr_reader

--- getting_started/tut_classes/stock_stats/book_in_stock.rb
+++ getting_started/tut_classes/stock_stats/book_in_stock.rb
@@ -1,4 +1,7 @@
 class BookInStock
+  attr_accessor :price
+  attr_reader :isbn
+
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
@@ -14,3 +17,9 @@ b2 = BookInStock.new('isbn2', 3.15)
 puts b2
 b3 = BookInStock.new('isbn3', 5.12)
 puts b3
+
+book = BookInStock.new('isbn10', 5.5)
+puts "ISBN - #{book.isbn}"
+puts "price - #{book.price}"
+book.price = book.price * 0.75
+puts "new price = #{book.price}"
