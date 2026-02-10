6617d4e509d40cb1d6fe887380bb727518ab91b7
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-10 16:05:10 -0300

    adiciona nosso programa principal que faz o uso das outras classes

--- getting_started/tut_classes/stock_stats/csv_reader.rb
+++ getting_started/tut_classes/stock_stats/csv_reader.rb
@@ -7,7 +7,7 @@ class CsvReader
   end
 
   def read_in_csv_data(csv_file_name)
-    CSV.foreach(csv_file_name, headers: true) do |raw|
+    CSV.foreach(csv_file_name, headers: true) do |row|
       @books_in_stock << BookInStock.new(row['ISBN'], row['Price'])
     end
   end
