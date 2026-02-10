43bb667224397a9adc1eb71249d64d8881910161
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-10 15:42:52 -0300

    adiciona classe CsvReader

--- getting_started/tut_classes/stock_stats/book_in_stock.rb
+++ getting_started/tut_classes/stock_stats/book_in_stock.rb
@@ -14,24 +14,4 @@ class BookInStock
   def price_in_cents
     (price * 100).round
   end
-
-  def price_in_cents=(cents)
-    @price = cents / 100
-  end
 end
-b1 = BookInStock.new('isbn1', 3)
-puts b1
-b2 = BookInStock.new('isbn2', 3.15)
-puts b2
-b3 = BookInStock.new('isbn3', 5.12)
-puts b3
-
-book = BookInStock.new('isbn10', 5.5)
-puts "ISBN - #{book.isbn}"
-puts "price - #{book.price}"
-book.price = book.price * 0.75
-puts "new price = #{book.price}"
-puts "price in cents = #{book.price_in_cents}"
-book.price_in_cents = 1234
-puts "price - #{book.price}"
-puts "price in cents = #{book.price_in_cents}"
