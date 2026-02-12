18e733046d6ea4221ea5c283b6e372b39eb5b418
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-11 23:15:04 -0300

    alguns ajustes na classe Produto

--- getting_started/claude_ai_exercises/exe_01/produto.rb
+++ getting_started/claude_ai_exercises/exe_01/produto.rb
@@ -1,5 +1,6 @@
 class Produto
   attr_reader :nome, :preco, :quantidade_estoque
+  attr_accessor :quantidade_estoque
 
   def initialize(nome, preco, quantidade_estoque)
     @nome = nome
@@ -14,7 +15,26 @@ class Produto
   def to_s
     "nome: #{@nome}, preco: #{@preco}, quantidade de estoque: #{@quantidade_estoque}"
   end
+
+  def aplicar_desconto(percentual)
+    @preco -= @preco * (percentual.to_f / 100)
+  end
+
+  def preco_em_centavos
+    (preco * 100).round
+  end
+
+  private
+
+  def validar_desconto(percentual)
+    return unless percentual < 0 || percentual > 100
+
+    puts 'percentual invalido'
+  end
 end
 
 produto1 = Produto.new('sabonete', 3.33, 10)
+puts produto1
+puts produto1.preco_em_centavos
+produto1.aplicar_desconto(30)
 puts produto1
