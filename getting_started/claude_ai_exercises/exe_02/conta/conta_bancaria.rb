e6687475834f7c95e2488b1b8d2ac2a6fc378fea
João Victor Bernardino de Lima <joaovictorbernardinodelima@gmail.com>
2026-02-12 10:48:07 -0300

    add implementacao conta_bancaria e teste

--- getting_started/claude_ai_exercises/exe_02/conta/conta_bancaria.rb
+++ getting_started/claude_ai_exercises/exe_02/conta/conta_bancaria.rb
@@ -7,17 +7,22 @@ class ContaBancaria
   end
 
   def depositar(valor)
+    puts "Deposito efetuado no valor de R$ #{valor}"
     @saldo += valor
   end
 
   def sacar(valor)
     puts 'valor inválido' if valor > @saldo
+
+    puts "Saque efetuado no valor de R$ #{valor}"
     @saldo -= valor
   end
 
   def transferir(valor, outra_conta)
     puts 'valor inválido para transferência' if valor > @saldo
     puts "enviando R$#{valor} para a outra conta #{outra_conta}"
+    sacar(valor)
+    outra_conta.depositar(valor)
   end
 
   def to_s
--- getting_started/claude_ai_exercises/exe_02/teste_conta.rb
+++ getting_started/claude_ai_exercises/exe_02/teste_conta.rb
@@ -1,8 +1,14 @@
 require_relative 'conta/conta_bancaria'
 
 conta1 = ContaBancaria.new('joão lima', 3450.4)
+conta2 = ContaBancaria.new('maria alice', 300.0)
 
+puts conta1
 conta1.depositar(10)
 puts conta1
 conta1.sacar(30)
 puts conta1
+
+conta1.transferir(200, conta2)
+puts conta1
+puts conta2
